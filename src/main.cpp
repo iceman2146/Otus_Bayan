6589ca5f22f5386393f57d3b5c0e6ada7f56a367
iceman2146 <iceman2146@mail.ru>
2023-09-25 22:17:40 +0300

    Add docs

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+/**
+ * @file main.cpp 
+ * 
+ */
 
+/**
+* @brief функция main
+* Основная функция main
+* @return возвращает код ошибки
+*/
 int main(int, char**){
     std::cout << "Hello, from Bayan!\n";
 }
